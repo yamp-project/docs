8f5f5589b6f122f612a953bea1a3d011714ae385
ramon.pawlowski <ramon.pawlowski@ise.de>
2025-04-08 11:13:44 +0200

    Init

---
+++ astro.config.mjs
@@ -0,0 +1,89 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+import starlightVersions from 'starlight-versions';
+import starlightHeadingBadges from 'starlight-heading-badges';
+
+// https://astro.build/config
+export default defineConfig({
+	integrations: [
+		starlight({
+			title: 'YAMP :: V',
+			social: {
+				github: 'https://github.com/withastro/starlight',
+			},
+			// Set English as the default language for this site.
+			defaultLocale: 'en',
+			locales: {
+				// English docs in `src/content/docs/en`
+				en: {
+					label: 'English',
+				},
+				// German docs in `src/content/docs/de/`
+				de: {
+					label: 'Deutsch',
+					lang: 'de',
+				}
+			},
+			sidebar: [
+				{
+					label: 'Appendix',
+					collapsed: false,
+					autogenerate: {
+						directory: "appendix",
+					}
+				},
+				{
+					label: 'Getting Started',
+					collapsed: false,
+					autogenerate: {
+						directory: "getting-started",
+					}
+				},
+				{
+					label: 'Developer Guides',
+					collapsed: true,
+					autogenerate: {
+						directory: "developer-guides",
+					}
+				},
+				{
+					label: 'GTA:V Reference Data',
+					collapsed: true,
+					autogenerate: {
+						directory: "gta-v-reference-data",
+					}
+				},
+				{
+					label: 'Platform Internals',
+					collapsed: true,
+					autogenerate: {
+						directory: "platform-internals",
+					}
+				},
+				{
+					label: 'SDK Development',
+					collapsed: true,
+					autogenerate: {
+						directory: "sdk-development",
+					}
+				},
+			],
+			plugins: [
+				starlightHeadingBadges(),
+				starlightVersions({
+					versions: [
+						{
+							slug: 'dev',
+							label: 'dev',
+						},
+						{
+							slug: 'rc',
+							label: 'rc',
+						},
+					],
+				}),
+			],
+		}),
+	],
+});
